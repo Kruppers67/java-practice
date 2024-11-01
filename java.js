a4cb9151b61de13180494132e685453bcdfbf4f5
kruppers67 <183672492+Kruppers67@users.noreply.github.com>
2024-11-01 16:19:28 -0600

    Finished game of rock paper scissors.

--- java.js
+++ java.js
@@ -1,22 +1,34 @@
+
+
 function pcChoice() {
     const choices = ['rock', 'paper', 'scissors'];
     return choices[Math.floor(Math.random() * choices.length)];
 }
 
+
+
 function theGame(_myChoice, _pcChoice) {
+
+
     if (_myChoice === _pcChoice) {
         return "Its a tie, you both picked the same item.";
     } else if (_myChoice === "rock" && _pcChoice === "scissors") {
+        humanScore++
         return "You win! rock beats scissors.";
     } else if (_myChoice === "paper" && _pcChoice === "rock") {
-        return "You win! paper beats rock.";
+        humanScore++
+        return "You win! paper beats rock."; 
     } else if (_myChoice === "scissors" && _pcChoice === "paper") {
+        humanScore++
         return "You win! scissors beats paper.";
     } else if (_myChoice === "scissors" && _pcChoice === "rock") {
+        pcScore++
         return "You lose! rock beats scissors.";
     } else if (_myChoice === "rock" && _pcChoice === "paper") {
+        pcScore++
         return "You lose! paper beats rock.";
     } else if (_myChoice === "paper" && _pcChoice === "scissors") {
+        pcScore++
         return "You lose! scissors beats paper.";
  
     
@@ -25,4 +37,21 @@ function theGame(_myChoice, _pcChoice) {
 }
 const _myChoice = prompt("Choose rock, paper, or scissors to start the game!").toLowerCase();
 const _pcChoice = pcChoice();
-console.log(theGame(_myChoice, _pcChoice))
+let humanScore = 0;
+let pcScore = 0;
+
+function game() {
+    for (let i=0; i<5; i++) {
+        theGame(_myChoice, _pcChoice)
+    }
+    if(humanScore > pcScore) {
+        return "You are smarter than a PC, congrats..."
+    } else if (humanScore < pcScore) {
+            return "You lose. Hah you doopid"
+    } else if (humanScore == pcScore) {
+            return "Welp, its a tie."
+    
+    
+    }
+}
+console.log(game());
